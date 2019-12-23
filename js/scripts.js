13d93251e7231cee7398e0089580fd0d5e277f42
gg-mg <ggichuki2017@gmail.com>
2019-12-23 12:44:12 +0300

    added disabled attribute to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,46 @@
 // business logic
 function EmailAddress(email, password) {
-    this.email = email;
-    this.password = password;
-  }
-  
-  EmailAddress.prototype.fullName = function() {
-    return this.email + " " + this.password;
-    //This is a method
-  };
-  //Property to be created
-  
-  // user interface logic
-  $(document).ready(function() {
-    $("form#new-email").submit(function(event) {
-      var inputtedemail = $("input#exampleInputEmail1").val();
-      var inputtedpassword = $("input#exampleInputPassword1").val();
-  
-      var newEmailAddress = new EmailAddress(inputtedemail, inputtedpassword);
-      alert(newEmailAddress.email);
-      alert(newEmailAddress.password);
-  
-      $("ul#contacts").append(
-        "<li><span class='contact'>" + newEmailAddress.email + "</span></li>"
-      );
-  
+  this.email = email;
+  this.password = password;
+}
+
+EmailAddress.prototype.fullName = function() {
+  return this.email + " " + this.password;
+  //This is a method
+};
+//Property to be created
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-email").submit(function(event) {
+    var inputtedemail = $("input#exampleInputEmail1").val();
+    var inputtedpassword = $("input#exampleInputPassword1").val();
+    var checksigned = $("input#exampleCheck1:checked").val();
+
+    var newEmailAddress = new EmailAddress(inputtedemail, inputtedpassword);
+    alert(newEmailAddress.email);
+    alert(newEmailAddress.password);
+
+    if (checksigned === "on") {
+      $("input#exampleInputEmail1").attr("disabled", true);
+      $("input#exampleInputPassword1").attr("disabled", true);
+    } else {
       //   This piece of code clears input fields (.val(""))
       $("input#exampleInputEmail1").val("");
       $("input#exampleInputPassword1").val("");
-  
-      // $(".contact").last().click(function () {
-      //   $("#show-contact").show();
-      //   $("#show-contact h2").text(newContact.firstName);
-      //   $(".first-name").text(newContact.firstName);
-      //   $(".last-name").text(newContact.lastName);
-      // });
-  
-      event.preventDefault();
-    });
+    }
+
+    $("ul#contacts").append(
+      "<li><span class='contact'>" + newEmailAddress.email + "</span></li>"
+    );
+
+    // $(".contact").last().click(function () {
+    //   $("#show-contact").show();
+    //   $("#show-contact h2").text(newContact.firstName);
+    //   $(".first-name").text(newContact.firstName);
+    //   $(".last-name").text(newContact.lastName);
+    // });
+
+    event.preventDefault();
   });
-  
+});
